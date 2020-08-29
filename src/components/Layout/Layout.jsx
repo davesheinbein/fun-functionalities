344ff090e69e29c78bcf1221108a255175f85d44
David Sheinbein <davesheinbein@gmail.com>
2020-08-28 18:50:52 -0700

    first 4 boxes added

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import BoxOne from '../BoxOne/BoxOne';
+import BoxTwo from '../BoxTwo/BoxTwo';
+import BoxThree from '../BoxThree/BoxThree';
+import './style/Layout.css';
 
 function Layout() {
-    return (
-        <div>
-            
-        </div>
-    )
+	return (
+		<div className='container'>
+			<BoxOne />
+			<BoxTwo />
+			<BoxThree />
+			<BoxTwo />
+		</div>
+	);
 }
 
-export default Layout
+export default Layout;
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import './App.css';
+import Layout from '../../components/Layout/Layout';
+import './style/App.css';
 class App extends Component {
- /*--- State ---*/
- /*--- Handle Methods ---*/
- /*--- Lifecycle Methods ---*/
- render() {
-   return (
-     <div className="App">
-       Hello World
-     </div>
-   );
- }
+	/*--- State ---*/
+	/*--- Handle Methods ---*/
+	/*--- Lifecycle Methods ---*/
+	render() {
+		return (
+			<div className='App'>
+				<Layout />
+			</div>
+		);
+	}
 }
 export default App;
