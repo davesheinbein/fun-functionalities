2bd440eee94603ac7c0ef24445b9f18420810da6
David Sheinbein <davesheinbein@gmail.com>
2020-09-01 16:40:02 -0700

    update

--- src/components/BoxThirteen/BoxThirteen.jsx
+++ src/components/BoxThirteen/BoxThirteen.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import Pong from 'react-pong';
+import * as React from 'react';
+import { Frame, Page } from 'framer';
+import {
+	QqOutlined,
+	LockOutlined,
+	MenuFoldOutlined,
+} from '@ant-design/icons';
 import './style/BoxThirteen.css';
 
-function BoxThirteen() {
+export function BoxThirteen() {
 	return (
-		<div>
-			{/* <Pong />, document.getElementById('container') */}
+		<div className='boxThirteenContainer'>
+			<Page width={150} height={150} radius={30}>
+				<Frame size={150} radius={30} background={'#111'}>
+					<QqOutlined />
+				</Frame>
+				<Frame size={150} radius={30} background={'#333'}>
+					<LockOutlined />
+				</Frame>
+				<Frame size={150} radius={30} background={'#fff'}>
+					<MenuFoldOutlined />
+				</Frame>
+			</Page>
 		</div>
 	);
 }
