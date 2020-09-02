91a899330a26970fefb6aa7ef2ab60c619444498
David Sheinbein <davesheinbein@gmail.com>
2020-09-01 17:49:30 -0700

    update to box 13

--- src/components/BoxThirteen/BoxThirteen.jsx
+++ src/components/BoxThirteen/BoxThirteen.jsx
@@ -2,23 +2,155 @@ import * as React from 'react';
 import { Frame, Page } from 'framer';
 import {
 	QqOutlined,
-	LockOutlined,
-	MenuFoldOutlined,
+	UserOutlined,
+	SmileOutlined,
+	SkinOutlined,
+	RocketOutlined,
+	ShoppingCartOutlined,
+	StarOutlined,
+	WifiOutlined,
+	ThunderboltOutlined,
 } from '@ant-design/icons';
 import './style/BoxThirteen.css';
 
 export function BoxThirteen() {
 	return (
 		<div className='boxThirteenContainer'>
-			<Page width={150} height={150} radius={30}>
+			<Page
+				currentPage={0}
+				width={150}
+				height={150}
+				radius={30}
+				alignment='center'
+				defaultEffect={'pile'}
+				direction='horizontal'
+				wheelEnabled={true}
+				animateCurrentPageUpdate={true}
+				momentum
+				onChangePage={(current, previous) => {
+					console.log(
+						current,
+						'<<< current',
+						previous,
+						'<<< previous'
+					);
+				}}>
 				<Frame size={150} radius={30} background={'#111'}>
-					<QqOutlined />
+					<div className='iconContainer'>
+						Hey
+						<QqOutlined
+							style={{
+								fill: 'blue',
+								color: '#fff',
+								width: '25%',
+								height: '25%',
+							}}
+						/>
+					</div>
+				</Frame>
+				<Frame size={150} radius={30} background={'#222'}>
+					<div className='iconContainer'>
+						Smile
+						<UserOutlined
+							style={{
+								fill: 'blue',
+								color: '#fff',
+								width: '25%',
+								height: '25%',
+							}}
+						/>
+					</div>
 				</Frame>
 				<Frame size={150} radius={30} background={'#333'}>
-					<LockOutlined />
+					<div className='iconContainer'>
+						Person
+						<SmileOutlined
+							style={{
+								fill: 'blue',
+								color: '#fff',
+								width: '25%',
+								height: '25%',
+							}}
+						/>
+					</div>
 				</Frame>
-				<Frame size={150} radius={30} background={'#fff'}>
-					<MenuFoldOutlined />
+				<Frame size={150} radius={30} background={'#444'}>
+					<div className='iconContainer'>
+						Hey
+						<SkinOutlined
+							style={{
+								fill: 'blue',
+								color: '#fff',
+								width: '25%',
+								height: '25%',
+							}}
+						/>
+					</div>
+				</Frame>
+				<Frame size={150} radius={30} background={'#555'}>
+					<div className='iconContainer'>
+						Smile
+						<RocketOutlined
+							style={{
+								fill: 'blue',
+								color: '#fff',
+								width: '25%',
+								height: '25%',
+							}}
+						/>
+					</div>
+				</Frame>
+				<Frame size={150} radius={30} background={'#666'}>
+					<div className='iconContainer'>
+						Person
+						<ShoppingCartOutlined
+							style={{
+								fill: 'blue',
+								color: '#fff',
+								width: '25%',
+								height: '25%',
+							}}
+						/>
+					</div>
+				</Frame>
+				<Frame size={150} radius={30} background={'#777'}>
+					<div className='iconContainer'>
+						Person
+						<StarOutlined
+							style={{
+								fill: 'blue',
+								color: '#fff',
+								width: '25%',
+								height: '25%',
+							}}
+						/>
+					</div>
+				</Frame>
+				<Frame size={150} radius={30} background={'#888'}>
+					<div className='iconContainer'>
+						Person
+						<WifiOutlined
+							style={{
+								fill: 'blue',
+								color: '#fff',
+								width: '25%',
+								height: '25%',
+							}}
+						/>
+					</div>
+				</Frame>
+				<Frame size={150} radius={30} background={'#999'}>
+					<div className='iconContainer'>
+						Person
+						<ThunderboltOutlined
+							style={{
+								fill: 'blue',
+								color: '#fff',
+								width: '25%',
+								height: '25%',
+							}}
+						/>
+					</div>
 				</Frame>
 			</Page>
 		</div>
