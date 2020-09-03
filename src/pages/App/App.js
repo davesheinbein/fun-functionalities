a69d7526b5be0dda67ffcc92ebaad3c20c3c7830
David Sheinbein <davesheinbein@gmail.com>
2020-09-03 13:00:54 -0700

    update to colors

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -10,9 +10,25 @@ ${(props) => {
 	switch (props.color) {
 		case 'black':
 			return `:root{
-			--mainColor: #000000;
+			--headerColor: #000000;
 			--titleColor: #000000;
 			--secondaryColor: #0B0A0A;
+
+			// BoxFour Colors
+			--colorOne: #8ced7d;
+			--colorTwo: #b3897b;
+			--colorThree: #98faff;
+			--colorFour: #d3ffa6;
+			--colorFive: #f3ccb9;
+			--colorSix: #aafcd2;
+			--colorSeven: #b98c87;
+			--colorEight: ##d4d8fe;
+
+			// Box Backgrounds
+			--boxTwoBg: radial-gradient(circle, rgba(255,199,123,1) 0%, rgba(224,148,233,1) 100%);
+			--boxThreeBg: radial-gradient(circle, rgba(233,216,148,1) 0%, rgba(123,189,255,1) 100%);
+			--boxFiveBg: linear-gradient(90deg, rgba(148,233,219,1) 0%, rgba(255,175,123,1) 100%);
+
 			--mainText: #ffffff;
 			--subText: rgba(255, 255, 255, .75);
 			--secondaryText: #ffffff;
@@ -34,9 +50,25 @@ ${(props) => {
 		default:
 		case 'default':
 			return `:root {
-			--mainColor:#ffffff;
+			--headerColor:#ffffff;
 			--titleColor: #F1F1F1;
 			--secondaryColor:#FFFCFC;
+
+			// BoxFour Colors
+			--colorOne: crimson;
+			--colorTwo: teal;
+			--colorThree: coral;
+			--colorFour: hotpink;
+			--colorFive: skyblue;
+			--colorSix: salmon;
+			--colorSeven: seagreen;
+			--colorEight: peachpuff;
+
+			// Box Backgrounds
+			--boxTwoBg: radial-gradient(circle, rgba(224,148,233,1) 0%, rgba(255,199,123,1) 100%);
+			--boxThreeBg: radial-gradient(circle, rgba(123,189,255,1) 0%, rgba(233,216,148,1) 100%);
+			--boxFiveBg: linear-gradient(90deg, rgba(255,175,123,1) 0%, rgba(148,233,219,1) 100%);
+
 			--mainText:#000000;
 			--subText: rgba(0, 0, 0, .75);
 			--secondaryText:#084181;
